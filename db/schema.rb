bafd1cc504f72ae6e5cc90cbe48ecc4ddb2ec258
Luan <a19300081@ceti.mx>
2024-02-08 17:45:36 -0600

    Connect to local db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_213020) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_231350) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "productos", force: :cascade do |t|
+    t.string "nombre"
+    t.string "descripcion"
+    t.string "precio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
